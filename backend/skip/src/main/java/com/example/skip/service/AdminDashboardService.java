67ed09ad2687740a8c032da5307b616c7886e4b6
Bong82 <jbp100@naver.com>
2025-06-04 10:10:24 +0900

    0604 dev 병합전

--- backend/skip/src/main/java/com/example/skip/service/AdminDashboardService.java
+++ backend/skip/src/main/java/com/example/skip/service/AdminDashboardService.java
@@ -31,8 +31,10 @@ public class AdminDashboardService {
         LocalDateTime to = end.plusDays(1).atStartOfDay();
 
         // 총 결제액, 총 결제건 수
-        double totalSales = Optional.ofNullable(paymentRepository.getTotalPaidPriceBetween(from, to)).orElse(0.0);
-        long totalSalesCount =  Optional.ofNullable(paymentRepository.getPaidCountBetween(from, to)).orElse(0L);
+        double totalSales = Optional.ofNullable(paymentRepository.getTotalPaidPriceBetween(from, to)).orElse(0.0)
+                            +Optional.ofNullable(paymentRepository.getCancelledSales(from, to)).orElse(0.0);
+        long totalSalesCount =  Optional.ofNullable(paymentRepository.getPaidCountBetween(from, to)).orElse(0L)
+                                +Optional.ofNullable(paymentRepository.getCancelledCount(from, to)).orElse(0L);
         // 렌탈샵 총 결제액 , 광고비 총 결제액
         double rentTotalSales = Optional.ofNullable(paymentRepository.getRentTotalSales(from, to)).orElse(0.0);
         double adTotalSales = Optional.ofNullable(paymentRepository.getAdTotalSales(from, to)).orElse(0.0);
@@ -47,8 +49,8 @@ public class AdminDashboardService {
         // 관리자 순수익액
         double profit = Optional.ofNullable(paymentRepository.getAdminProfit(from, to)).orElse(0.0);
         // 결제승인건
-        long successCount = Optional.ofNullable(paymentRepository.getPaidCountBetween(from, to)).orElse(0L)
-                            - Optional.ofNullable(paymentRepository.getCancelledCount(from, to)).orElse(0L);
+        long successCount = Optional.ofNullable(paymentRepository.getPaidCountBetween(from, to)).orElse(0L);
+                            //- Optional.ofNullable(paymentRepository.getCancelledCount(from, to)).orElse(0L);
         // 결제취소금액, 결제취소건
         double cancelPrice = Optional.ofNullable(paymentRepository.getCancelledSales(from, to)).orElse(0.0);
         long cancelCount = Optional.ofNullable(paymentRepository.getCancelledCount(from, to)).orElse(0L);
--- frontend/skip/src/components/adminpage/UserApprovalTable.jsx
+++ frontend/skip/src/components/adminpage/UserApprovalTable.jsx
@@ -61,6 +61,7 @@ const UserApprovalTable = ({
                   <td>{user.name}</td>
                   <td>{user.phone}</td>
                   <td>{user.biz_status}</td>
+                  <td>{user.biz_status}</td>
                   <td>{user.email}</td>
                   <td>{formatDate(user.regdate)}</td>
                   <td>
