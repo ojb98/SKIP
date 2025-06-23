cea2d59fc2ea39da9e3075a976e1f2da36cc5ada
Bong82 <jbp100@naver.com>
2025-06-23 12:19:06 +0900

    배너요청, 부스트요청폼css 수정

--- frontend/skip/src/services/admin/rent/AdService.js
+++ frontend/skip/src/services/admin/rent/AdService.js
@@ -1,54 +1,54 @@
-import axios from 'axios';
+import caxios from '../../../api/caxios';
 
 
 export const fetchCash = async (userId, rentId) => {
-  const resp = await axios.get('/api/rentAdmin/cash', { params: { userId, rentId } });
+  const resp = await caxios.get('/api/rentAdmin/cash', { params: { userId, rentId } });
   return resp.data.remainingCash;
 };
 
 export const chargeCash = async data => {
-  const resp = await axios.post('/api/rentAdmin/cash', data);
+  const resp = await caxios.post('/api/rentAdmin/cash', data);
   return resp.data.remainingCash;
 };
 
 export const fetchCpb = async (userId, rentId) => {
-  const resp = await axios.get('/api/rentAdmin/boost/cpb', { params: { userId, rentId } });
+  const resp = await caxios.get('/api/rentAdmin/boost/cpb', { params: { userId, rentId } });
   return resp.data.cpb;
 };
 
 export const decryptCash = async cashToken => {
-  const resp = await axios.get('/api/rentAdmin/cash/decrypt', { params: { cashToken } });
+  const resp = await caxios.get('/api/rentAdmin/cash/decrypt', { params: { cashToken } });
   return resp.data.remainingCash;
 };
 
 
 export const fetchActiveBoost = async (userId, rentId) => {
-  const resp = await axios.get('/api/rentAdmin/boost', { params: { userId, rentId } });
+  const resp = await caxios.get('/api/rentAdmin/boost', { params: { userId, rentId } });
   return resp.data.activeBoost;
 };
 
 export const purchaseBoost = async (userId, rentId, boost, cpb, cashToken) => {
-  const resp = await axios.post('/api/rentAdmin/boost', { userId, rentId, boost, cpb, cashToken });
+  const resp = await caxios.post('/api/rentAdmin/boost', { userId, rentId, boost, cpb, cashToken });
   return resp.data.remainingCash;
 };
 
 export const submitBannerRequest = async (formData, cashToken) => {
   formData.append('cashToken', cashToken);
-  const resp = await axios.post('/api/rentAdmin/banner', formData, {
+  const resp = await caxios.post('/api/rentAdmin/banner', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return resp.data.remainingCash;
 };
 
 export const fetchBannerDetail = async (userId, waitingId) => {
-  const resp = await axios.get(`/api/rentAdmin/banner/${waitingId}`, {
+  const resp = await caxios.get(`/api/rentAdmin/banner/${waitingId}`, {
     params: { userId },
   });
   return resp.data;
 };
 
 export const fetchWithdrawnBanner = async userId => {
-  const resp = await axios.get('/api/rentAdmin/banner/withdrawn', {
+  const resp = await caxios.get('/api/rentAdmin/banner/withdrawn', {
     params: { userId },
   });
   return resp.data;
@@ -64,7 +64,7 @@ export const resubmitBannerRequest = async (
   form.append('userId', userId);
   form.append('cpcBid', cpcBid);
   if (bannerImage) form.append('bannerImage', bannerImage);
-  const resp = await axios.put(`/api/rentAdmin/banner/${waitingId}`, form, {
+  const resp = await caxios.put(`/api/rentAdmin/banner/${waitingId}`, form, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return resp.data;
